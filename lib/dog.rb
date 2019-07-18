2e417b258d64be5c3b5561a9306ed8019055b06b
John Odom <john.osborne.odom@gmail.com>
2019-07-18 15:30:05 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Add your code here
+require 'pry'
+class Dog
+
+    @@all =[]
+
+    def initialize(name)
+        @name=name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def self.clear_all
+        @@all.clear
+    end
+    def name
+        @name
+    end
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+end
